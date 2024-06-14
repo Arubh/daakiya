3b2a05d53d1dcd17e8a12d3c01451839755c9495
Aditya <iit2022003@iiitl.ac.in>
2024-06-14 22:26:26 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Input from './components/Input';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+
+      <div class="container">
+        <h1 class="my-3">Welcome to Daakiya</h1>
+    </div>
+
+    <div className="container">
+      <Input />
+    </div>
     </div>
   );
 }
